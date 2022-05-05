97abc59b95f63ff72e1134002cdfec81fcc744e5
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-05 16:17:39 +0200

    #cisonoriuscito#vogliopiangere#attimidigloria

--- snack-2/main.js
+++ snack-2/main.js
@@ -3,14 +3,22 @@
 // Ogni volta che ne crei uno, stampalo a schermo.
 
 // creo un prompt che chiede all utente un numero
-
 let userInput = Number(prompt("Digita un numero"));
 
+// il numero diventa N array
+for ( let i = 0; i < userInput; i++){
+    const array = [];
 
-// il numero diventa il nome dell'array
-// ogni array sarà composto da 10 numeri casuali tra 1 e 100
-// stampo il risultato
+    // ogni array sarà composto da 10 numeri casuali tra 1 e 100
+    while (array.length < 10) {
+        array.push(Number(Math.floor((Math.random() * 100) + 1)));
+    }
+
+    // stampo il risultato
+    console.log(array);
+}
 
 
 
-console.log(userInput);
+
+
