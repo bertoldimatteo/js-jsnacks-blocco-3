e1dfff362921c32d35d30efc003eb50998ffd253
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-05 17:10:28 +0200

    analizzo problema

--- snack-3/main.js
+++ snack-3/main.js
@@ -3,8 +3,23 @@
 // Partendo da un array di numeri, 
 // stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
+const arrayNumbers = [1,3,5,8,12,20,60,52,14,17,63,89];
 
+const greenNumbers = [];
+const redNumbers = [];
 
+const greenText = document.getElementById("green").style.color = "green";
+const redText = document.getElementById("red").style.color = "red";
+
+for (let i = 0; i < arrayNumbers.length; i++) {
+    if (arrayNumbers % 2 == 0) {
+        greenNumbers.push(arrayNumbers);
+        const pushGreen = document.getElementById("green").innerHTML = greenNumbers;
+    } else if ( arrayNumbers % 2 == 1) {
+        redNumbers.push(arrayNumbers);
+        const pushRed = document.getElementById("red").innerHTML = redNumbers;
+    }
+}
 
 
 
